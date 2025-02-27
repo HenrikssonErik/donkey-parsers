db4216abc2ea4adcb42b920d6b80280e0b5d4451
HenrikssonErik <erik.henr00@gmail.com>
2025-02-27 17:25:09 +0100

    Changes in the cadets directory to support the new file structure and generate a better makefile. More changes will come

--- cadets/make_gen.py
+++ cadets/make_gen.py
@@ -1,63 +1,56 @@
-import sys, os
+import sys
 import math
 
 def truncate_run(fh):
-	"""
-	This is a generic function that truncates the json file and runs libpvm on the truncated data.
-	@fh is the handle of the generated Makefile 
-	"""
-	fh.write("define truncate_data\n")
-	fh.write("\tsed -n '$(1),$(2)p;$(3)q' $(4).json | ../../libpvm-rs/build/pvm2csv - ../../../data/$(6)/$(4)_$(5).zip ; \\\n")
-	fh.write("\tmkdir ../../../data/$(6)/$(4)_$(5) ; \\\n")
-	fh.write("\tunzip ../../../data/$(6)/$(4)_$(5).zip -d ../../../data/$(6)/$(4)_$(5) ; \\\n")
-	fh.write("\trm ../../../data/$(6)/$(4)_$(5).zip\n")
-	fh.write("endef\n")
+    """
+    Function that defines how to truncate data in the Makefile.
+    """
+    fh.write("define truncate_data\n")
+    fh.write("\tsed -n '$(1),$(2)p;$(3)q' ../../../data/cadets-e3/$(6)/$(4).json | ../../libpvm-rs/build/pvm2csv - ../../../data/cadets-e3/$(6)/$(4)_$(5).zip ; \\\n")
+    fh.write("\tmkdir -p ../../../data/cadets-3/$(6)/$(4)_$(5) ; \\\n")
+    fh.write("\tunzip ../../../data/cadets-e3/$(6)/$(4)_$(5).zip -d ../../../data/cadets-e3/$(6)/$(4)_$(5) ; \\\n")
+    fh.write("\trm ../../../data/cadets-e3/$(6)/$(4)_$(5).zip\n")
+    fh.write("endef\n")
 
 def gen(filepath, name, chunks, ty, fh):
-	"""
-	This function takes a file @filepath with shortened name @name, calculate the places to truncate the file based on the number of chunks @chunks.
-	Then it writes to the output file using its handle @fh.
-	@ty determines it is "benign" or "attack"
-	"""
-	with open(filepath, "r") as f:
-		for i, l in enumerate(f):
-			pass
-	lc = i + 1	# total line counts of the file
-	f.close()
-	fh.write(name + ":\n")
+    """
+    Generates makefile content for each dataset chunk.
+    """
+    try:
+        with open(filepath, "r") as f:
+            for i, l in enumerate(f):
+                pass
+        lc = i + 1  # Total line count
+    except FileNotFoundError:
+        print(f"Error: File {filepath} not found!")
+        return
 
-	interval = int(math.ceil(lc / int(chunks)))
-	start = 1
-	end = lc
-	cnt = 0
-	while start + interval < end:
-		fh.write("\t$(call truncate_data,")
-		fh.write(str(start) + "," + str(start + interval) + ",")
-		start = start + interval + 1
-		fh.write(str(start) + "," + name + "," + str(cnt) + "," + ty + ")\n")
-		cnt += 1
-	fh.write("\t$(call truncate_data,")
-	fh.write(str(start) + "," + str(end) + "," + str(end + 1) + "," + name + "," + str(cnt) + "," + ty + ")\n")
+    fh.write(name + ":\n")
 
+    interval = int(math.ceil(lc / int(chunks)))
+    start = 1
+    end = lc
+    cnt = 0
+
+    while start + interval < end:
+        fh.write(f"\t$(call truncate_data,{start},{start + interval},{start + interval + 1},{name},{cnt},{ty})\n")
+        start = start + interval + 1
+        cnt += 1
+
+    fh.write(f"\t$(call truncate_data,{start},{end},{end + 1},{name},{cnt},{ty})\n")
 
 if __name__ == "__main__":
-	if (len(sys.argv) < 1):
-		print("""
-			Usage: python make_gen.py
-		"""
-		)
-		sys.exit(1)
+    print("Starting Makefile generation...")
 
-	makefile = open("Makefile", "a+")
-	truncate_run(makefile)
-	makefile.write("\n")
-	gen("../../../data/benign/benign1.json", "benign1", 50, "benign", makefile)
-	makefile.write("\n")
-	gen("../../../data/benign/benign2.json", "benign2", 10, "benign", makefile)
-	makefile.write("\n")
-	gen("../../../data/benign/benign3.json", "benign3", 50, "benign", makefile)
-	makefile.write("\n")
-	gen("../../../data/attack/pandex.json", "pandex", 25, "attack", makefile)
-	
-	
-	
+    with open("Makefile", "w") as makefile:
+        truncate_run(makefile)
+        makefile.write("\n")
+        gen("../../../data/cadets-e3/benign/benign1.json", "benign1", 50, "benign", makefile)
+        makefile.write("\n")
+        gen("../../../data/cadets-e3/benign/benign2.json", "benign2", 10, "benign", makefile)
+        makefile.write("\n")
+        gen("../../../data/cadets-e3/benign/benign3.json", "benign3", 50, "benign", makefile)
+        makefile.write("\n")
+        gen("../../../data/cadets-e3/attack/pandex.json", "pandex", 25, "attack", makefile)
+
+    print("Makefile successfully created!")
