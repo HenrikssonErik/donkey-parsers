ce4b2ed6c6aaf0371faad72364855d653552910e
HenrikssonErik <erik.henr00@gmail.com>
2025-03-26 14:09:40 +0100

    Converted the src/dst paths to ids after sorting the records list by timestamp, so the src/dst starts at 0 in the output base file.

--- atlas/atlas_parser.py
+++ atlas/atlas_parser.py
@@ -2,165 +2,190 @@ import math
 import argparse, json, os
 import utils.constants as constants
 
-"""
-    Each type has process_path as its source except the scriptload
-    since it does not have a child but only parent and process
-"""
-def get_src(atlas_record):
-    if atlas_record['type'] == 'endpoint.event.scriptload':
-        return atlas_record['parent_path']
+def get_src(src_atlas_record):
+    """
+        Each type has process_path as its source except the scriptload
+        since it does not have a child but only parent and process
+    """
+    if src_atlas_record['type'] == 'endpoint.event.scriptload':
+        return src_atlas_record['parent_path']
     
-    return atlas_record['process_path']
+    return src_atlas_record['process_path']
 
-""" 
-    Each 'type' has a different dst and dst_type.
-    The destination is collected from different attributes in the jsonl file.
-    The destionation type is a single letter:
-        'process': 'a'
-        'file': 'c'
-        'NA': 'e'
-"""
-def get_destionation(atlas_record):
-    match atlas_record['type']:
+def get_destionation(dst_atlas_record):
+    """ 
+        Each 'type' has a different dst and dst_type.
+        The destination is collected from different attributes in the jsonl file.
+        The destionation type is a single letter:
+            'process': 'a'
+            'file': 'c'
+            'NA': 'e'
+    """
+    match dst_atlas_record['type']:
         case 'endpoint.event.filemod':
             return {
-                'dst': atlas_record['filemod_name'],
+                'dst': dst_atlas_record['filemod_name'],
                 'dst_type': 'c'
                 }
         case 'endpoint.event.regmod':
             return {
-                'dst': atlas_record['regmod_name'],
+                'dst': dst_atlas_record['regmod_name'],
                 'dst_type': 'c'
             }           
         case 'endpoint.event.moduleload':
             return {
-                'dst': atlas_record['modload_name'],
+                'dst': dst_atlas_record['modload_name'],
                 'dst_type': 'c'
             }   
         case 'endpoint.event.procend' | 'endpoint.event.procstart':
             return {
-                'dst': atlas_record['childproc_name'],
+                'dst': dst_atlas_record['childproc_name'],
                 'dst_type': 'a'
             }   
         case 'endpoint.event.crossproc':
             return {
-                'dst': atlas_record['crossproc_name'],
+                'dst': dst_atlas_record['crossproc_name'],
                 'dst_type': 'a'
             } 
         case 'endpoint.event.scriptload':
             return {
-                'dst': atlas_record['process_path'],
+                'dst': dst_atlas_record['process_path'],
                 'dst_type': 'a'
             } 
         case 'endpoint.event.netconn': 
             return {
-                'dst': atlas_record['remote_ip'],
+                'dst': dst_atlas_record['remote_ip'],
                 'dst_type': 'e'
             }   
 
-# Converts the src/dst path to a number, for easier handling.
-def get_path_id(path, seen_paths, no_paths):
-    new_path = 0
-    if path not in seen_paths:
-        seen_paths[path] = no_paths
-        no_paths += 1
-        new_path = 1
-
-    return seen_paths[path], no_paths, new_path
-
-""" 
-    An action in the jsonl file can contain several action in one,
-    e.g., "action":"ACTION_FILE_CREATE | ACTION_FILE_MOD_OPEN | ACTION_FILE_OPEN_WRITE"
-    These need to be split into 3 different records to better group the data.
-"""
-def split_actions_and_add_to_records(value_dict, records):
-        actions = value_dict['edge_type'].split('|')
-        above_second = False
-
-        for action in actions:
-            action = constants.actionDict[action.strip()]
-
-            # It cannot be a new new src/dst since the first action has seen the path before.
-            if above_second:
-                value_dict['new_src'] = 0               
-                value_dict['new_dst'] = 0
-
-            value_dict['edge_type'] = action
-            records.append(value_dict)
-            above_second = True
-
-        return records
-
-# Writes to the output files, depending on the base length (--length input). 
-"""
-    Writes to the output files, depending on the base length (--length input). 
-"""
-def write_to_file(base_outfile, stream_outfile, records, base_length):
-    count = 0
-    for value_dict in records:
-        if count < base_length:
-            base_outfile.write(f"{value_dict['src']} {value_dict['dst']} {value_dict['src_type']}"+ 
-                    f":{value_dict['dst_type']}:{value_dict['edge_type']}:{count}\n")
-        else: 
-            stream_outfile.write(f"{value_dict['src']} {value_dict['dst']} {value_dict['src_type']}"+ 
-                    f":{value_dict['dst_type']}:{value_dict['edge_type']}:{value_dict['new_src']}"+
-                    f":{value_dict['new_dst']}:{count}\n")
-        count += 1
-
-        if count % 50000 == 0:
-            print("Lines written: ", count)
-
-# Main method that converts the atlas jsonl file to the parsed txt StreamSpot format.
-def convert_file_to_streamspot(file, base_output, stream_output, base_graph_size):
- 
-    input_file = open(file, 'r')
-    base_outfile = open(base_output, 'w+')
-    stream_outfile = open(stream_output, 'w+')
-
-    seen_paths = dict()
-    no_paths = 0
+def create_records(input_file):
+    """
+        Reads each line/object in the input file and converts the object to
+        a record containing the necessary information. Returns the complete
+        list of individual records. 
+    """
     records = []
-    print('Begin reading and parsing file.')
 
     for line in input_file:
         atlas_record = json.loads(line.strip())
         destination = get_destionation(atlas_record)
         src = get_src(atlas_record)
 
-        src_id, no_paths, new_src = get_path_id(src, seen_paths, no_paths)
-        dst_id, no_paths, new_dst = get_path_id(destination['dst'], seen_paths, no_paths)
-
-        value_dict = {
-            'src': src_id,
+        record_dict = {
+            'src': src,
             'src_type': 'a',
-            'dst': dst_id,
+            'dst': destination['dst'],
             'dst_type': destination['dst_type'],
             'edge_type': atlas_record['action'],
             'device_timestamp': atlas_record['device_timestamp'],
-            'unknown': '?',
             'graph_id': atlas_record['schema'],
-            'new_src': new_src,
-            'new_dst': new_dst
+            'new_src': 0, #Placeholder
+            'new_dst': 0 # Placeholder
         }
 
-        records = split_actions_and_add_to_records(value_dict, records)
+        records.extend(split_actions_and_add_to_records(record_dict))
+
+    return records
+
+def split_actions_and_add_to_records(single_record_dict):
+    """ 
+        An action in the jsonl file can contain several action in one,
+        e.g., "action":"ACTION_FILE_CREATE | ACTION_FILE_MOD_OPEN | ACTION_FILE_OPEN_WRITE"
+        These need to be split into 3 different records to better group the data.
+    """
+    actions = single_record_dict['edge_type'].split('|')
+    split_single_records = []
+
+    for action in actions:
+        translated_action = constants.actionDict[action.strip()]
+
+        new_record = single_record_dict.copy()
+        new_record['edge_type'] = translated_action
+        split_single_records.append(new_record)
+
+    return split_single_records
+
+def get_path_id(path, seen_paths, path_id):
+    """
+        Converts the src/dst path to a number, for easier handling and to comply with
+        the Unicorn format.
+    """
+    new_path = 0
+
+    if path not in seen_paths:
+        seen_paths[path] = path_id
+        path_id += 1
+        new_path = 1
+    
+    return seen_paths[path], path_id, new_path
+
+def sort_records(sorted_records):
+    """
+        Sorts the records (each object) first by their device_timestamp and 
+        sorts the src/dst ids according to the new list order so the first 
+        id in the output starts at 0.
+    """
+    seen_paths = dict()
+    path_id = 0
+    sorted_records = sorted(sorted_records, key=lambda record: record['device_timestamp'])
+
+    for sorted_record in sorted_records:
+        src_id, path_id, new_src = get_path_id(sorted_record['src'], seen_paths, path_id)
+        dst_id, path_id, new_dst = get_path_id(sorted_record['dst'], seen_paths, path_id)
+
+        sorted_record['src'] = src_id
+        sorted_record['dst'] = dst_id
+        sorted_record['new_src'] = new_src
+        sorted_record['new_dst'] = new_dst
+
+    return sorted_records
+
+def write_to_file(base_outfile, stream_outfile, records, base_length):
+    """
+        Writes to the output files, depending on the base length (--length input).
+        The first -l records are written to the base file and the rest will be written
+        to the stream file.  
+    """
+    count = 0
+    for record in records:
+        if count < base_length:
+            base_outfile.write(f"{record['src']} {record['dst']} {record['src_type']}"+ 
+                    f":{record['dst_type']}:{record['edge_type']}:{count}\n")
+        else: 
+            stream_outfile.write(f"{record['src']} {record['dst']} {record['src_type']}"+ 
+                    f":{record['dst_type']}:{record['edge_type']}:{record['new_src']}"+
+                    f":{record['new_dst']}:{count}\n")
+        
+        count += 1
+        if count % 50000 == 0:
+            print("Lines written: ", count)
+
+def convert_file_to_streamspot(file, base_output, stream_output, base_length):
+    """ Main method that converts the atlas jsonl file to the parsed txt Unicorn format. """
+    input_file = open(file, 'r')
+    base_outfile = open(base_output, 'w+')
+    stream_outfile = open(stream_output, 'w+')
+
+    print('Begin reading and parsing file.')
+    records = create_records(input_file)
 
     print('Begin sorting records.')
-    records_sorted = sorted(records, key=lambda record: record['device_timestamp'])
+    sorted_records = sort_records(records)
+
     print('Begin writing to files.')
-    write_to_file(base_outfile, stream_outfile, records_sorted, base_graph_size)
+    write_to_file(base_outfile, stream_outfile, sorted_records, base_length)
 
     input_file.close()
     base_outfile.close()
     stream_outfile.close()
 
-# Read the number of rows in a file to calculate the base graph size.
 def get_file_length(input_file):
+    """ Read the number of rows in a file to calculate the base graph size. """
     with open(input_file, 'r') as fp:
         return len(fp.readlines())
 
-# Creates the stream and base directory.
 def create_directories(directory):
+    """ Creates the stream and base directory. """
     directories = ['stream', 'base']
     print(directory)
     for dir in directories:
